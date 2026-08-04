b42f1cf72fdc75fac5dd5d0b240e771574c8f5c8
morsalalaameri44-cmyk <morsalalaameri44@gmail.com>
2026-08-04 04:13:14 +0300

    update Ckeke/src/CakeModel.jsx

--- Ckeke/src/CakeModel.jsx
+++ Ckeke/src/CakeModel.jsx
@@ -1,37 +1,35 @@
 import { useOrderStore } from './store';
 
 export default function CakeModel() {
+  // جلب قائمة الطبقات من المخزن الذي أنشأناه للتو
   const layers = useOrderStore((state) => state.layers);
 
   return (
-    <group position={[0, -1.2, 0]}> 
+    // خفضنا المجسم قليلاً للأسفل (-1) لكي يظل في منتصف الشاشة عندما نصل لـ 3 طبقات
+    <group position={[0, -1, 0]}> 
       
-      {/* قاعدة الكيك الذهبية */}
-      <mesh position={[0, 0, 0]} receiveShadow castShadow>
-        <cylinderGeometry args={[2.7, 2.7, 0.1, 64]} />
-        <meshStandardMaterial color="#d4af37" metalness={0.6} roughness={0.3} />
-      </mesh>
-
-      {/* طبقات الكيك - أسطوانة نظيفة ومثالية بدون أي تعقيدات */}
       {layers.map((layer, index) => {
-        const yPos = (index * 1.2) + 0.65; 
+        // حساب الارتفاع (Y): كل طبقة ارتفاعها 1.2
+        // الطبقة الأولى (index 0) مركزها عند 0.6
+        // الطبقة الثانية (index 1) مركزها عند 1.8
+        // وهكذا... لترتكز كل طبقة فوق الأخرى بدقة متناهية
+        const yPos = (index * 1.2) + 0.6;
 
         return (
           <mesh key={layer.id} position={[0, yPos, 0]} castShadow receiveShadow>
-            {/* أسطوانة بدقة 64 لتكون دائرية تماماً وناعمة */}
-            <cylinderGeometry args={[layer.radius, layer.radius, layer.height, 64]} />
+            {/* استخدام نصف القطر (radius) القادم من المخزن لكل طبقة */}
+            <cylinderGeometry args={[layer.radius, layer.radius, layer.height, 128]} />
             
-            {/* استخدام خامة فيزيائية تعطي مظهر الكريمة الفاخرة المطفية */}
-            <meshPhysicalMaterial 
+            <meshStandardMaterial 
               color={layer.color} 
-              roughness={0.9} 
-              metalness={0.05}
-              clearcoat={0.1} 
-              clearcoatRoughness={0.8}
+              roughness={0.6} // ملمس واقعي يشبه الكريمة
+              metalness={0.1}
+              envMapIntensity={0.5} 
             />
           </mesh>
         );
       })}
+
     </group>
   );
 }
