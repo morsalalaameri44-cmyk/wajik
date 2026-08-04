a727236deb71a3f1ccdb80f6a659c2fd1344ab36
morsalalaameri44-cmyk <morsalalaameri44@gmail.com>
2026-08-04 04:20:14 +0300

    update Ckeke/src/CakeModel.jsx

--- Ckeke/src/CakeModel.jsx
+++ Ckeke/src/CakeModel.jsx
@@ -1,28 +1,37 @@
 import { useOrderStore } from './store';
-import { useTexture } from '@react-three/drei';
 
 export default function CakeModel() {
-  const cakeColor = useOrderStore((state) => state.cakeConfig.color);
+  const layers = useOrderStore((state) => state.layers);
 
   return (
-    <group position={[0, -0.5, 0]}>
-      {/* جسم الكيكة الرئيسي - أسطوانة عالية الدقة */}
-      <mesh castShadow receiveShadow>
-        {/* زيادة عدد الـ radial segments إلى 128 لتنعيم الحواف تماماً */}
-        <cylinderGeometry args={[2.1, 2.1, 1.2, 128]} />
-        <meshStandardMaterial 
-          color={cakeColor || '#FFD700'} 
-          roughness={0.6} // جعل السطح أقل لمعاناً لتبدو كالكريمة
-          metalness={0.1}
-          envMapIntensity={0.5} // تفاعل جيد مع إضاءة البيئة
-        />
-      </mesh>
+    <group position={[0, -1.2, 0]}> 
       
-      {/* ظل خفيف إضافي تحت المجسم لتأكيد مكانه في الفراغ */}
-      <mesh rotation-x={-Math.PI / 2} position-y={-0.6} receiveShadow>
-        <planeGeometry args={[5, 5]} />
-        <shadowMaterial opacity={0.3} />
+      {/* قاعدة الكيك الذهبية */}
+      <mesh position={[0, 0, 0]} receiveShadow castShadow>
+        <cylinderGeometry args={[2.7, 2.7, 0.1, 64]} />
+        <meshStandardMaterial color="#d4af37" metalness={0.6} roughness={0.3} />
       </mesh>
+
+      {/* طبقات الكيك - أسطوانة نظيفة ومثالية بدون أي تعقيدات */}
+      {layers.map((layer, index) => {
+        const yPos = (index * 1.2) + 0.65; 
+
+        return (
+          <mesh key={layer.id} position={[0, yPos, 0]} castShadow receiveShadow>
+            {/* أسطوانة بدقة 64 لتكون دائرية تماماً وناعمة */}
+            <cylinderGeometry args={[layer.radius, layer.radius, layer.height, 64]} />
+            
+            {/* استخدام خامة فيزيائية تعطي مظهر الكريمة الفاخرة المطفية */}
+            <meshPhysicalMaterial 
+              color={layer.color} 
+              roughness={0.9} 
+              metalness={0.05}
+              clearcoat={0.1} 
+              clearcoatRoughness={0.8}
+            />
+          </mesh>
+        );
+      })}
     </group>
   );
 }
