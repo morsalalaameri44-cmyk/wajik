e4a54ad6a4584ee4d9b4f7f072ea5c927e8b14ee
morsalalaameri44-cmyk <morsalalaameri44@gmail.com>
2026-08-06 05:45:05 +0300

    update Ckeke/src/FinalCake.jsx

--- Ckeke/src/FinalCake.jsx
+++ Ckeke/src/FinalCake.jsx
@@ -1,27 +1,34 @@
+import React from 'react';
 import { useOrderStore } from './store';
 
 export default function FinalCake() {
   const layers = useOrderStore((state) => state.layers);
 
-  return (
-    <group position={[0, -1.2, 0]}> 
-      
-      {/* قاعدة الكيك الذهبية */}
-      <mesh position={[0, 0, 0]} receiveShadow castShadow>
-        <cylinderGeometry args={[2.7, 2.7, 0.1, 64]} />
-        <meshStandardMaterial color="#d4af37" metalness={0.6} roughness={0.3} />
-      </mesh>
+  // إعدادات هندسية دقيقة لتراص الطبقات بدون فراغات مع تناقص الحجم
+  const layerSettings = [
+    { radius: 2.2, height: 1.5, yPos: 0.75 }, // الطبقة الأولى (السفلية - الأكبر)
+    { radius: 1.7, height: 1.5, yPos: 2.25 }, // الطبقة الثانية (الوسطى)
+    { radius: 1.2, height: 1.5, yPos: 3.75 }  // الطبقة الثالثة (العلوية - الأصغر)
+  ];
 
-      {/* طبقات الكيك الصافية - أسطوانة نقية فقط */}
+  return (
+    <group position={[0, -1.2, 0]}>
       {layers.map((layer, index) => {
-        const yPos = (index * 1.2) + 0.65; 
+        const setting = layerSettings[index];
         return (
-          <mesh key={`layer-${layer.id}`} position={[0, yPos, 0]} castShadow receiveShadow>
-            <cylinderGeometry args={[layer.radius, layer.radius, layer.height, 64]} />
-            <meshStandardMaterial color={layer.color} roughness={0.8} metalness={0.1} />
+          <mesh key={layer.id} position={[0, setting.yPos, 0]} castShadow receiveShadow>
+            {/* 64 لضمان دائرية فائقة النعومة للمجسم */}
+            <cylinderGeometry args={[setting.radius, setting.radius, setting.height, 64]} />
+            <meshStandardMaterial color={layer.color} roughness={0.4} metalness={0.1} />
           </mesh>
         );
       })}
+      
+      {/* الصحن الذهبي الفاخر (القاعدة) */}
+      <mesh position={[0, -0.05, 0]} receiveShadow castShadow>
+        <cylinderGeometry args={[3.2, 3.2, 0.1, 64]} />
+        <meshStandardMaterial color="#d4af37" roughness={0.2} metalness={0.8} />
+      </mesh>
     </group>
   );
 }
