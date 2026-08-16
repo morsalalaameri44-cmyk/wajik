5669c9264d2b59a4eb609db834af70a57dadb3e9
morsalalaameri44-cmyk <morsalalaameri44@gmail.com>
2026-08-16 05:40:29 +0300

    Update store.js

--- Ckeke/src/store.js
+++ Ckeke/src/store.js
@@ -1,27 +1,49 @@
 import { create } from 'zustand';
 
-export const useOrderStore = create((set) => ({
-  // ... البيانات السابقة (CustomerData, FlavorData)
-  customerData: { name: '', phone: '', occasion: 'عيد ميلاد', deliveryDate: '', deliveryTime: '', notes: '' },
-  updateCustomerData: (field, value) => set((state) => ({ customerData: { ...state.customerData, [field]: value } })),
+const LAYER_PRICE = 5000; 
 
+export const useOrderStore = create((set) => ({
+  // بيانات العميل
+  customerData: { name: '', phone: '', occasion: 'عيد ميلاد', deliveryDate: '', deliveryTime: '', notes: '' },
+  updateCustomerData: (field, value) => set((state) => ({
+    customerData: { ...state.customerData, [field]: value }
+  })),
+
+  // النكهات والحشوات
   flavorData: { cakeFlavor: 'فانيليا', filling: 'شوكولاتة', topping: 'كريمة زبدة' },
-  updateFlavorData: (field, value) => set((state) => ({ flavorData: { ...state.flavorData, [field]: value } })),
+  updateFlavorData: (field, value) => set((state) => ({
+    flavorData: { ...state.flavorData, [field]: value }
+  })),
 
   // المجسم
   layers: [{ id: 1, color: '#fcd34d' }],
+  addLayer: () => set((state) => {
+    if (state.layers.length < 3) {
+      return { layers: [...state.layers, { id: state.layers.length + 1, color: '#ffffff' }] };
+    }
+    return state;
+  }),
+  removeLayer: () => set((state) => {
+    if (state.layers.length > 1) {
+      return { layers: state.layers.slice(0, -1) };
+    }
+    return state;
+  }),
   updateLayerColor: (index, color) => set((state) => {
     const newLayers = [...state.layers];
     newLayers[index].color = color;
     return { layers: newLayers };
   }),
 
-  // [جديد] محرك الزينة
-  decorations: [], // مصفوفة ستخزن { type: 'rose', position: [x,y,z], id: 1 }
+  // الزينة (المحرك الجديد)
+  decorations: [],
   addDecoration: (type) => set((state) => ({
     decorations: [...state.decorations, { id: Date.now(), type, position: [0, 1.5, 0] }]
   })),
   removeDecoration: (id) => set((state) => ({
     decorations: state.decorations.filter((d) => d.id !== id)
   })),
+
+  // السعر
+  getTotalPrice: (state) => state.layers.length * LAYER_PRICE
 }));
