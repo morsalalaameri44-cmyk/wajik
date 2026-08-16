0a5304955553446c044e4d4373498fd95ab38e89
morsalalaameri44-cmyk <morsalalaameri44@gmail.com>
2026-08-16 06:23:50 +0300

    Refactor decoration rendering and comments in FinalCake

--- Ckeke/src/FinalCake.jsx
+++ Ckeke/src/FinalCake.jsx
@@ -14,23 +14,22 @@ export default function FinalCake() {
           <meshStandardMaterial color={layer.color} roughness={0.4} metalness={0.1} />
         </mesh>
       ))}
-
-      {/* محرك الزينة الاحترافي مع السحب والإفلات */}
+      
+      {/* الزينة القابلة للسحب */}
       {decorations.map((dec) => (
         <TransformControls key={dec.id} mode="translate">
-          <group position={[0, 2.5, 0]}>
-            {/* هنا نضع "شكل الزينة" */}
-            <Float speed={2} rotationIntensity={0.5} floatIntensity={0.5}>
+          <Float speed={2} rotationIntensity={0.5} floatIntensity={0.5}>
+            <group position={[0, 2.5, 0]}>
               {dec.type === '🌹 ورد' && <Text fontSize={1}>🌹</Text>}
               {dec.type === '👑 تاج' && <Text fontSize={1}>👑</Text>}
               {dec.type === '🦋 فراشات' && <Text fontSize={1}>🦋</Text>}
               {dec.type === '✍️ كتابة' && <Text fontSize={0.5} color="black">نص</Text>}
-            </Float>
-          </group>
+            </group>
+          </Float>
         </TransformControls>
       ))}
 
-      {/* القاعدة الذهبية */}
+      {/* القاعدة */}
       <mesh position={[0, -0.05, 0]} receiveShadow castShadow>
         <cylinderGeometry args={[3.2, 3.2, 0.1, 64]} />
         <meshStandardMaterial color="#d4af37" roughness={0.2} metalness={0.8} />
