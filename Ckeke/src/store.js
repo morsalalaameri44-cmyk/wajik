4a70eac4b2080873a16bf7ea03855f8757e9b863
morsalalaameri44-cmyk <morsalalaameri44@gmail.com>
2026-08-16 05:35:18 +0300

    Update store.js

--- Ckeke/src/store.js
+++ Ckeke/src/store.js
@@ -1,51 +1,27 @@
 import { create } from 'zustand';
 
-const LAYER_PRICE = 5000; 
-
 export const useOrderStore = create((set) => ({
-  // 1. بيانات العميل
-  customerData: {
-    name: '',
-    phone: '',
-    occasion: 'عيد ميلاد',
-    deliveryDate: '',
-    deliveryTime: '',
-    notes: ''
-  },
-  updateCustomerData: (field, value) => set((state) => ({
-    customerData: { ...state.customerData, [field]: value }
-  })),
+  // ... البيانات السابقة (CustomerData, FlavorData)
+  customerData: { name: '', phone: '', occasion: 'عيد ميلاد', deliveryDate: '', deliveryTime: '', notes: '' },
+  updateCustomerData: (field, value) => set((state) => ({ customerData: { ...state.customerData, [field]: value } })),
 
-  // 2. النكهات والحشوات
-  flavorData: {
-    cakeFlavor: 'فانيليا',
-    filling: 'شوكولاتة',
-    topping: 'كريمة زبدة'
-  },
-  updateFlavorData: (field, value) => set((state) => ({
-    flavorData: { ...state.flavorData, [field]: value }
-  })),
+  flavorData: { cakeFlavor: 'فانيليا', filling: 'شوكولاتة', topping: 'كريمة زبدة' },
+  updateFlavorData: (field, value) => set((state) => ({ flavorData: { ...state.flavorData, [field]: value } })),
 
-  // 3. مجسم التورتة
+  // المجسم
   layers: [{ id: 1, color: '#fcd34d' }],
-  addLayer: () => set((state) => {
-    if (state.layers.length < 3) {
-      return { layers: [...state.layers, { id: state.layers.length + 1, color: '#ffffff' }] };
-    }
-    return state;
-  }),
-  removeLayer: () => set((state) => {
-    if (state.layers.length > 1) {
-      return { layers: state.layers.slice(0, -1) };
-    }
-    return state;
-  }),
   updateLayerColor: (index, color) => set((state) => {
     const newLayers = [...state.layers];
     newLayers[index].color = color;
     return { layers: newLayers };
   }),
 
-  // حساب السعر الإجمالي
-  getTotalPrice: (state) => state.layers.length * LAYER_PRICE
+  // [جديد] محرك الزينة
+  decorations: [], // مصفوفة ستخزن { type: 'rose', position: [x,y,z], id: 1 }
+  addDecoration: (type) => set((state) => ({
+    decorations: [...state.decorations, { id: Date.now(), type, position: [0, 1.5, 0] }]
+  })),
+  removeDecoration: (id) => set((state) => ({
+    decorations: state.decorations.filter((d) => d.id !== id)
+  })),
 }));
