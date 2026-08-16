7ca578a7fd42cf766168649bd80c382ac0583936
morsalalaameri44-cmyk <morsalalaameri44@gmail.com>
2026-08-16 06:21:46 +0300

    Update FinalCake.jsx

--- Ckeke/src/FinalCake.jsx
+++ Ckeke/src/FinalCake.jsx
@@ -1,51 +1,33 @@
 import React from 'react';
 import { useOrderStore } from './store';
-import { Text } from '@react-three/drei';
+import { Text, TransformControls, Float } from '@react-three/drei';
 
 export default function FinalCake() {
   const { layers, decorations } = useOrderStore();
 
   return (
     <group position={[0, -1.2, 0]}>
-      {/* 1. رسم طبقات التورتة */}
-      {layers.map((layer, index) => {
-        const radius = Math.max(2.2 - (index * 0.5), 0.5); 
-        const yPos = 0.75 + (index * 1.5);
-        return (
-          <mesh key={index} position={[0, yPos, 0]} castShadow receiveShadow>
-            <cylinderGeometry args={[radius, radius, 1.5, 64]} />
-            <meshStandardMaterial color={layer.color} roughness={0.4} metalness={0.1} />
-          </mesh>
-        );
-      })}
-      
-      {/* 2. محرك الزينة */}
+      {/* طبقات الكيك */}
+      {layers.map((layer, index) => (
+        <mesh key={index} position={[0, 0.75 + (index * 1.5), 0]} castShadow receiveShadow>
+          <cylinderGeometry args={[Math.max(2.2 - (index * 0.5), 0.5), Math.max(2.2 - (index * 0.5), 0.5), 1.5, 64]} />
+          <meshStandardMaterial color={layer.color} roughness={0.4} metalness={0.1} />
+        </mesh>
+      ))}
+
+      {/* محرك الزينة الاحترافي مع السحب والإفلات */}
       {decorations.map((dec) => (
-        <group key={dec.id} position={[0, 3.2, 0]}> 
-          {dec.type === '🌹 ورد' && (
-            <mesh>
-              <sphereGeometry args={[0.3]} />
-              <meshStandardMaterial color="red" />
-            </mesh>
-          )}
-          {dec.type === '👑 تاج' && (
-            <mesh rotation={[Math.PI / 2, 0, 0]}>
-              <torusGeometry args={[0.3, 0.1, 16, 32]} />
-              <meshStandardMaterial color="gold" metalness={1} />
-            </mesh>
-          )}
-          {dec.type === '🦋 فراشات' && (
-            <mesh>
-              <coneGeometry args={[0.2, 0.4, 3]} />
-              <meshStandardMaterial color="purple" />
-            </mesh>
-          )}
-          {dec.type === '✍️ كتابة' && (
-            <Text position={[0, 0, 0]} fontSize={0.5} color="black" anchorX="center" anchorY="middle">
-              نص
-            </Text>
-          )}
-        </group>
+        <TransformControls key={dec.id} mode="translate">
+          <group position={[0, 2.5, 0]}>
+            {/* هنا نضع "شكل الزينة" */}
+            <Float speed={2} rotationIntensity={0.5} floatIntensity={0.5}>
+              {dec.type === '🌹 ورد' && <Text fontSize={1}>🌹</Text>}
+              {dec.type === '👑 تاج' && <Text fontSize={1}>👑</Text>}
+              {dec.type === '🦋 فراشات' && <Text fontSize={1}>🦋</Text>}
+              {dec.type === '✍️ كتابة' && <Text fontSize={0.5} color="black">نص</Text>}
+            </Float>
+          </group>
+        </TransformControls>
       ))}
 
       {/* القاعدة الذهبية */}
